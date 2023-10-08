247ddf72331cf4f512cadf351f1c662f51d9c944
Fariha1865 <farihaamin187@gmail.com>
2023-10-09 02:44:00 +0600

    Error  page implemented

---
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,31 @@
+import { useNavigate, useRouteError } from "react-router-dom";
+
+const ErrorPage = () => {
+
+    const error = useRouteError();
+    const navigate = useNavigate();
+
+    const goBack = () => {
+
+        navigate("/");
+    }
+    return (
+        <div>
+            <h1 className="text-3xl font-bold text-center mt-14">Ooopsss!!! Page Not Found</h1>
+
+            {
+                <div className="flex justify-center mt-5">
+
+                    <button onClick={goBack} className="btn bg-[#752727] text-white hover:bg-[#752727] mb-10">Go Back</button>
+
+                </div>
+            }
+            <div className="flex justify-center h-80">
+                <img src="https://i.ibb.co/PQ2nntf/error.gif" alt="" />
+            </div>
+
+        </div>
+    );
+};
+
+export default ErrorPage;
--- src/Routes/Route.jsx
+++ src/Routes/Route.jsx
@@ -10,11 +10,13 @@ import PrivateRoute from "./PrivateRoute";
 import Contact from "../Pages/Contact/Contact";
 import Gallery from "../Pages/Gallery/Gallery";
 import AboutUs from "../Pages/AboutUs/AboutUs";
+import ErrorPage from "../Pages/ErrorPage/ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root></Root>,
+    errorElement: <ErrorPage></ErrorPage>,
 
     children: [
          {
